576485ce6f25dd432b01fcae2ae74a12abf37ae4
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-09-28 21:23:26 -0600

    Create/remove user test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,28 @@
 module Main where
 
+import qualified Data.List as List
+import Lib
 import Test.Hspec
+import qualified Test.QuickCheck as QC
 
--- import qualified Test.QuickCheck as QC
 main :: IO ()
 main =
     hspec $ do
+        describe "Create/remove user" $
+            it "Creates/removes the correct user" $
+            QC.ioProperty $ do
+                usr <- QC.generate $ QC.listOf QC.arbitrary
+                pwd <- QC.generate $ QC.listOf QC.arbitrary
+                u <- users
+                createUser usr pwd
+                u' <- users
+                removeUser usr pwd
+                return $ usr `elem` u' && List.delete usr u' == u
+        describe "Adding services" $
+            it "Correctly add the service" $ True `shouldBe` True
         describe "Changing passwords" $ do
             it "Service password" $ True `shouldBe` True
             it "PGM password" $ True `shouldBe` True
         describe "Changing usernames" $ do
             it "Service username" $ True `shouldBe` True
             it "PGM username" $ True `shouldBe` True
-        describe "These are tests for the second property" $ do
-            it "Property2 requirement 1" $ True `shouldBe` True
-            it "Property2 requirement 2" $ True `shouldBe` True
